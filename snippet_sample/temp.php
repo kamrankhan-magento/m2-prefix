9bc617b62a2f3385faf383d574b531451236f5cb
Zain ul abidin <zain@aligent.com.au>
2018-04-22 20:40:05 +0930

    temp: improved way of DI with easy scope hinting

--- snippet_sample/temp.php
+++ snippet_sample/temp.php
@@ -1,7 +1,19 @@
 <?php
 
 setStateAdminHtml();
-$model = getObjectFromName('\NameSpace\Module\Model\File');
-echo "<pre>";
-$data = $model->getSomeData();
-print_r($data);
+
+Class baseUrl{
+    function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        d($scopeConfig->getValue('web/unsecure/base_url'));
+    }
+};
+getObjectFromName('\baseUrl');
+
+/**
+ * Alternative approach without DI
+ */
+
+/** @var \Magento\Framework\App\Config\ScopeConfigInterface $configAnotherWay */
+$configAnotherWay = getObjectFromName('\Magento\Framework\App\Config\ScopeConfigInterface');
+d($configAnotherWay->getValue('web/unsecure/base_url'));
